a0df9816558a6f73dbff5dff63ec920dbb954475
Rodrigo Avancini <rodrigo.avancini@unesp.br>
2022-11-29 21:18:58 -0300

    15. 3Sum

---
+++ 15_3Sum/15.py
@@ -0,0 +1,13 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        tri = []
+        for i in range(len(nums)-2):
+            for j in range(i+1, len(nums)-1):
+                for k in range(j+1, len(nums)):
+                    l = [nums[i],nums[j],nums[k]]
+                    if (sum(l) == 0):
+                        l = sorted(l)
+                        if not l in tri:
+                            tri.append(l)    
+
+        return tri
