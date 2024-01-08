21fdd38b55394ef163993d0a570a81030599dd8a
Rodrigo Avancini <rodrigo.avancini@thoughtful.ai>
2024-01-08 12:00:12 -0300

    Jan 8 Daily Challenge

--- 0900_1000/938_Range_Sum_of_BST/938.py
+++ 0900_1000/938_Range_Sum_of_BST/938.py
@@ -1,3 +1,5 @@
+# ========= ATTEMPT FROM Dec 07, 2022 ========= #
+
 # Definition for a binary tree node.
 from typing import Optional
 
@@ -18,3 +20,28 @@ class Solution:
         elif root.val > high:
             return self.rangeSumBST(root.left, low, high)
         return root.val + self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)
+
+
+# ========= ATTEMPT FROM Jan 08, 2024 (Daily Question) ========= #
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int, sum=0) -> int:
+        if root.val is None:
+            return sum
+        if low <= root.val <= high:
+            sum += root.val
+
+        if root.left is not None:
+            sum = self.rangeSumBST(root.left, low, high, sum)
+        if root.right is not None:
+            sum = self.rangeSumBST(root.right, low, high, sum)
+                
+        return sum
+
+        
--- 0900_1000/938_Range_Sum_of_BST/README.md
+++ 0900_1000/938_Range_Sum_of_BST/README.md
@@ -7,7 +7,8 @@ Given the `root` node of a binary search tree and two integers `low` and `high`,
 
 ## Submissions Status
 
-| Language | Status   | Runtime | Memory |
-| -------- | -------- | ------- | ------ |
-| python3  | Accepted | 291     | 23MB   |
+| Language | Status   | Runtime                                    | Memory                                       | Date         |
+| -------- | -------- | ------------------------------------------ | -------------------------------------------- | ------------ |
+| python3  | Accepted | 291                                        | 23MB                                         | Dec 07, 2022 |
+| python3  | Accepted | 117ms (Beats 92.37% of users with Python3) | 24.40MB (Beats 97.66% of users with Python3) | Jan 08, 2024 |
 
