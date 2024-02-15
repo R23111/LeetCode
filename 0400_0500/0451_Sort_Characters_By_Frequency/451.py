eadbec024f9ea90452143de609ec905fac6b4356
Rodrigo Avancini <rodrigo.avancini@thoughtful.ai>
2024-02-15 18:13:51 -0300

    451

--- 0400_0500/0451_Sort_Characters_By_Frequency/451.py
+++ 0400_0500/0451_Sort_Characters_By_Frequency/451.py
@@ -11,3 +11,16 @@ class Solution:
                 ans += i[0]
 
         return ans
+
+
+# Problem of the day Feb 7th, 2024
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        occ = []
+        for c in set(s):
+            occ.append((c, s.count(c)))
+        occ = sorted(occ, key=lambda x: x[1], reverse=True)
+        return "".join(c[0]*c[1] for c in occ)
+
+        
